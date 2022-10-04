391b37f4cfd88a638038eae27b208b39090de852
BulanAyuPratama31 <110890756+BulanAyuPratama31@users.noreply.github.com>
2022-10-04 15:02:57 +0700

    Add files via upload

---
+++ koneksi-daftar.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$_SERVER = "localhost";
+$user = "root";
+$password = "";
+$nama_database = "pendaftaran";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+if(!$db){
+    die("Gagal terhubung dengan database". mysqli_connect_error());
+}
+?>
+<!--Nama: Bulan Ayu Pratama-->
